9aae11a046d083887572a53e2caa625489c3d73f
keea <heea44@naver.com>
2018-07-09 17:18:12 +0900

    ver.1
    Play Selected Particles

---
+++ ParticleSimulation/Assets/Editor/ParticlePlayer.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditorInternal;
+using System.Linq;
+using System;
+
+public class TimeControl
+{
+    private double m_lastFrameEditorTime = 0;
+
+    //재생중인지 판단
+    public bool IsPlaying { get; private set; }
+    //시간의 하한
+    public float minTime { get; private set; }
+    //시간의 상한
+    public float maxTime { get; private set; }
+
+    //재생속도
+    public float speed { get; set; }
+
+    public float currentTime;
+
+    public float getCurrentTime()
+    {
+        return Mathf.Repeat(currentTime, maxTime);
+
+    }
+
+public void SetMinMaxTime(float minTime, float maxTime)
+    {
+        this.minTime = minTime;
+        this.maxTime = maxTime;
+    }
+
+
+    public TimeControl()
+    {
+        EditorApplication.update += TimeUpdate;
+    }
+
+    public void TimeUpdate()
+    {
+        if (IsPlaying)
+        {
+            var timeSinceStartup = EditorApplication.timeSinceStartup;
+            var deltaTime = timeSinceStartup - m_lastFrameEditorTime;
+            m_lastFrameEditorTime = timeSinceStartup;
+
+            currentTime += (float)deltaTime * speed;
+        }
+    }
+
+    //재생
+    public void Play()
+    {
+        IsPlaying = true;
+        m_lastFrameEditorTime = EditorApplication.timeSinceStartup;
+    }
+
+    //일시 정지
+    public void Pause()
+    {
+        IsPlaying = false;
+    }
+
+    //정지
+    public void Stop()
+    {
+        IsPlaying = false;
+        currentTime = 0;
+    }
+}
+
+public class ParticlePlayer : EditorWindow{
+
+    ParticleControl particleControl;
+    TimeControl timeControl;
+
+    [MenuItem("K_Particle/Player")]
+    static void open()
+    {
+      
+       GetWindow<ParticlePlayer>();
+    }
+
+    void OnEnable()
+    {
+        timeControl = new TimeControl();
+        timeControl.SetMinMaxTime(0, 10);
+        timeControl.speed = 0.1f;
+
+        GameObject particleManager = Resources.Load("Prefabs/K_ParticleManager/ParticleManager") as GameObject;
+        particleControl = particleManager.GetComponent<ParticleControl>();
+        particleControl.init();
+    }
+
+    void OnGUI()
+    {
+        if (particleControl == null)
+            return;
+
+        //플레이버튼
+        var buttonText = timeControl.IsPlaying ? "Pause" : "Play";
+        if (GUILayout.Button(buttonText))
+        {
+            if (timeControl.IsPlaying)
+                timeControl.Pause();
+            else
+                timeControl.Play();
+        }
+
+        timeControl.currentTime = GUILayout.HorizontalSlider(timeControl.getCurrentTime(),
+            timeControl.minTime, timeControl.maxTime, "box", "box", GUILayout.Height(40), GUILayout.ExpandWidth(true));
+
+        //GUI갱신
+        if (timeControl.IsPlaying)
+            Repaint();
+
+        if (particleControl.arr_name.Count() > 0)
+        {
+            //널이 아닐 경우
+            for (int i = 0; i < particleControl.amount; i++)
+            {
+                particleControl.playlist[i] = 
+                    GUILayout.Toggle(particleControl.playlist[i], particleControl.arr_name[i],
+                    EditorStyles.miniButton,
+                    GUILayout.MaxWidth(position.width / 3));
+                if (particleControl.playlist[i])
+                {
+                    particleControl.particles[i].Simulate(timeControl.getCurrentTime());
+                }
+                else
+                    particleControl.particles[i].Simulate(0);
+            }
+        }
+
+        //화살표 키로 시간이동
+        if(Event.current.type == EventType.KeyDown)
+        {
+            //재생 중이면 일시정지함.
+            timeControl.Pause();
+
+            if (Event.current.keyCode == KeyCode.RightArrow)
+                timeControl.currentTime += 0.01f;
+
+            if (Event.current.keyCode == KeyCode.LeftArrow)
+                timeControl.currentTime -= 0.01f;
+
+            GUI.changed = true;
+            Event.current.Use();
+            Repaint();
+        }
+    }
+}
---
+++ ParticleSimulation/Assets/Scripts/ParticleControl.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleControl : MonoBehaviour {
+    public  int amount;
+    public  List<string> arr_name;
+    public  List<bool> playlist;
+    public List<ParticleSystem> particles;
+
+    public ParticleControl()
+    {
+        arr_name = new List<string>();
+        playlist = new List<bool>();
+        particles = new List<ParticleSystem>();
+        amount = 0; 
+    }
+
+    public void init()
+    {
+        amount = 0;
+        arr_name.Clear();
+        playlist.Clear();
+
+        //씬 내의 ParticleSystem를 모두 얻어오기
+        var particleSystems = FindObjectsOfType<ParticleSystem>();
+        foreach (var particleSystem in particleSystems)
+        {
+            if (IsRoot(particleSystem))
+            {
+                arr_name.Add(particleSystem.name);
+                playlist.Add(false);
+                particles.Add(particleSystem);
+                amount++;
+            }
+        }
+    }
+
+
+    //부모의 ParticleSystem인지 아닌지를 판단하기
+    bool IsRoot(ParticleSystem ps)
+    {
+        var parent = ps.transform.parent;
+        //부모 없는 particleSystem이면 경로
+        if (parent == null)
+            return true;
+
+        //부모가 있어도 ParticleSystem 컴포넌트가 아니면 루트
+        return parent.GetComponent<ParticleSystem>() == false;
+    }
+}
