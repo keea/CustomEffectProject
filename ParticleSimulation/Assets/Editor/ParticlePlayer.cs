7d35f01c9b38725dcf1ff14de6865bee39dae609
keea <heea44@naver.com>
2018-07-27 15:51:04 +0900

    ver1.1.2

--- ParticleSimulation/Assets/Editor/ParticlePlayer.cs
+++ ParticleSimulation/Assets/Editor/ParticlePlayer.cs
@@ -6,73 +6,6 @@ using UnityEditorInternal;
 using System.Linq;
 using System;
 
-public class TimeControl
-{
-    private double m_lastFrameEditorTime = 0;
-
-    //재생중인지 판단
-    public bool IsPlaying { get; private set; }
-    //시간의 하한
-    public float minTime { get; private set; }
-    //시간의 상한
-    public float maxTime { get; private set; }
-
-    //재생속도
-    public float speed { get; set; }
-
-    public float currentTime;
-
-    public float getCurrentTime()
-    {
-        return Mathf.Repeat(currentTime, maxTime);
-
-    }
-
-public void SetMinMaxTime(float minTime, float maxTime)
-    {
-        this.minTime = minTime;
-        this.maxTime = maxTime;
-    }
-
-
-    public TimeControl()
-    {
-        EditorApplication.update += TimeUpdate;
-    }
-
-    public void TimeUpdate()
-    {
-        if (IsPlaying)
-        {
-            var timeSinceStartup = EditorApplication.timeSinceStartup;
-            var deltaTime = timeSinceStartup - m_lastFrameEditorTime;
-            m_lastFrameEditorTime = timeSinceStartup;
-
-            currentTime += (float)deltaTime * speed;
-        }
-    }
-
-    //재생
-    public void Play()
-    {
-        IsPlaying = true;
-        m_lastFrameEditorTime = EditorApplication.timeSinceStartup;
-    }
-
-    //일시 정지
-    public void Pause()
-    {
-        IsPlaying = false;
-    }
-
-    //정지
-    public void Stop()
-    {
-        IsPlaying = false;
-        currentTime = 0;
-    }
-}
-
 public class ParticlePlayer : EditorWindow
 {
 
@@ -237,7 +170,7 @@ public class ParticlePlayer : EditorWindow
         var lastRect = GUILayoutUtility.GetLastRect();
         float time = 
         EditorGUI.FloatField(new Rect((position.width / 2) - 40,
-            lastRect.y, 80, EditorGUIUtility.singleLineHeight), timeControl.currentTime);
+            lastRect.y, 80 ,EditorGUIUtility.singleLineHeight) ,timeControl.currentTime);
 
         timeControl.currentTime = time;
     }
---
+++ ParticleSimulation/Assets/Scripts/TimeControl.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class TimeControl
+{
+    private double m_lastFrameEditorTime = 0;
+
+    //재생중인지 판단
+    public bool IsPlaying { get; private set; }
+    //시간의 하한
+    public float minTime { get; private set; }
+    //시간의 상한
+    public float maxTime { get; private set; }
+
+    //재생속도
+    public float speed { get; set; }
+
+    public float currentTime;
+
+    public float getCurrentTime()
+    {
+        return Mathf.Repeat(currentTime, maxTime);
+
+    }
+
+    public void SetMinMaxTime(float minTime, float maxTime)
+    {
+        this.minTime = minTime;
+        this.maxTime = maxTime;
+    }
+
+
+    public TimeControl()
+    {
+        EditorApplication.update += TimeUpdate;
+    }
+
+    public void TimeUpdate()
+    {
+        if (IsPlaying)
+        {
+            var timeSinceStartup = EditorApplication.timeSinceStartup;
+            var deltaTime = timeSinceStartup - m_lastFrameEditorTime;
+            m_lastFrameEditorTime = timeSinceStartup;
+
+            currentTime += (float)deltaTime * speed;
+        }
+    }
+
+    //재생
+    public void Play()
+    {
+        IsPlaying = true;
+        m_lastFrameEditorTime = EditorApplication.timeSinceStartup;
+    }
+
+    //일시 정지
+    public void Pause()
+    {
+        IsPlaying = false;
+    }
+
+    //정지
+    public void Stop()
+    {
+        IsPlaying = false;
+        currentTime = 0;
+    }
+}
